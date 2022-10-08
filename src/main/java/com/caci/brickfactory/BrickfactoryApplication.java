1fb69a639ea2f5cabb101c694b3609b88b235c09
Becienzo <vincenzo.iovine94@gmail.com>
2022-10-08 10:49:45 +0100

    set up of Maven project with Spring boot

---
+++ src/main/java/com/caci/brickfactory/BrickfactoryApplication.java
@@ -0,0 +1,13 @@
+package com.caci.brickfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickfactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrickfactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caci/brickfactory/BrickfactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caci.brickfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrickfactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
